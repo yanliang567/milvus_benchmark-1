8edf4d7e15b70f44d6e2498644fb3e2073aa5aa4
zw <zw@milvus.io>
2021-02-19 14:12:24 +0800

    use douban
    Signed-off-by: zw <zw@milvus.io>

--- ci/jenkinsfile/deploy_test.groovy
+++ ci/jenkinsfile/deploy_test.groovy
@@ -8,8 +8,8 @@ try {
         print "Git clone url: ${TEST_URL}:${TEST_BRANCH}"
         checkout([$class: 'GitSCM', branches: [[name: "${TEST_BRANCH}"]], doGenerateSubmoduleConfigurations: false, extensions: [], submoduleCfg: [], userRemoteConfigs: [[credentialsId: "${params.GIT_USER}", url: "${TEST_URL}", name: 'origin', refspec: "+refs/heads/${TEST_BRANCH}:refs/remotes/origin/${TEST_BRANCH}"]]])
         print "Install requirements"
-        // sh "python3 -m pip install -r requirements.txt -i http://pypi.douban.com/simple --trusted-host pypi.douban.com"
-        sh "python3 -m pip install -r requirements.txt"
+        sh "python3 -m pip install -r requirements.txt -i http://pypi.douban.com/simple --trusted-host pypi.douban.com"
+        // sh "python3 -m pip install -r requirements.txt"
         sh "export KUBECONFIG=/root/kube/.kube/config && python3 main.py --image-version=${params.IMAGE_VERSION} --schedule-conf=scheduler/${params.CONFIG_FILE} --deploy-mode=${params.DEPLOY_MODE}"
     }
 } catch (exc) {
