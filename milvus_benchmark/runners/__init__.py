1b96d98a1ae5e9b50cecbd6172c2a73e37b1299d
ThreadDao <yufen.zong@zilliz.com>
2021-03-22 19:45:17 +0800

    add ann_accuracy
    Signed-off-by: ThreadDao <yufen.zong@zilliz.com>

--- milvus_benchmark/runners/__init__.py
+++ milvus_benchmark/runners/__init__.py
@@ -3,6 +3,7 @@ from .locust import LocustInsertRunner, LocustSearchRunner, LocustRandomRunner
 from .search import SearchRunner, InsertSearchRunner
 from .build import BuildRunner, InsertBuildRunner
 from .accuracy import AccuracyRunner
+from .accuracy import AccAccuracyRunner
 
 
 def get_runner(name, env, metric):
@@ -15,5 +16,6 @@ def get_runner(name, env, metric):
         "locust_random_performance": LocustRandomRunner(env, metric),
         "insert_build_performance": InsertBuildRunner(env, metric),
         "build_performance": BuildRunner(env, metric),
-        "accuracy": AccuracyRunner(env, metric)
+        "accuracy": AccuracyRunner(env, metric),
+        "ann_accuracy": AccAccuracyRunner(env, metric)
     }.get(name)
