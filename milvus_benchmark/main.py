3bdf4b0a73d6a187e2b694d0563babbf7f5ef663
zhenwu <zhenwu@milvus.io>
2021-05-19 14:11:23 +0800

    upload
    Signed-off-by: zhenwu <zhenwu@milvus.io>

--- milvus_benchmark/main.py
+++ milvus_benchmark/main.py
@@ -104,8 +104,8 @@ def run_suite(run_type, suite, env_mode, env_params):
                     case_metric.update_status(status="RUN_FAILED")
                     case_metric.update_message(err_message)
                 logger.debug(case_metric.metrics)
-                if env_mode == "helm":
-                    api.save(case_metric)
+                # if env_mode == "helm":
+                api.save(case_metric)
         else:
             logger.info("Deploy failed on server")
             metric.update_status(status="DEPLOYE_FAILED")
