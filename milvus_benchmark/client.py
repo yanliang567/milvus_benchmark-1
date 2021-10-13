a2bf3e42941b836422fcd7542f2e455332f39887
wt <ting.wang@zilliz.com>
2021-10-13 21:13:26 +0800

    Update client.py

--- milvus_benchmark/client.py
+++ milvus_benchmark/client.py
@@ -306,8 +306,8 @@ class MilvusClient(object):
             "bool": {"must": must_params}
         }
 
-        logger.debug("[Testing] query param of search: %s" % str(query))
-        utils.search_param_analysis(vector_query, filter_query)
+        # logger.debug("[Testing] query param of search: %s" % str(query))
+        # utils.search_param_analysis(vector_query, filter_query)
         result = self._milvus.search(tmp_collection_name, query, timeout=timeout)
 
         # result = self._milvus.search(tmp_collection_name, query, timeout=timeout)
