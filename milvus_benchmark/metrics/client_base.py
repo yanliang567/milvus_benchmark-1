28c393e2b8f5c8d8264357e4782376768dbf908a
wangting0128 <ting.wang@zilliz.com>
2021-10-19 11:56:41 +0800

    add logs

---
+++ milvus_benchmark/metrics/client_base.py
@@ -0,0 +1,19 @@
+
+class ClientBase:
+    def __init__(self):
+        pass
+
+    def query(self):
+        pass
+
+    def insert(self):
+        pass
+
+    def delete(self):
+        pass
+
+    def exist(self):
+        pass
+
+    def create(self):
+        pass
