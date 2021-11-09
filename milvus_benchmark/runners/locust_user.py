2ce1007ab27ae0a658a5c4ad752aaaf381b2d842
wangting0128 <ting.wang@zilliz.com>
2021-11-09 19:15:32 +0800

    update ni_per

--- milvus_benchmark/runners/locust_user.py
+++ milvus_benchmark/runners/locust_user.py
@@ -71,12 +71,12 @@ def locust_executor(host, port, collection_name, connection_type="single", run_p
         ni_per = MyUser.params["insert"]["ni_per"]
         _nq = ni_per + 10 if ni_per > nq else _nq
 
-        logger.debug("[locust_executor] ni_per of insert : %s" % str(ni_per))
+        logger.debug("[locust_executor] ni_per of insert : %s" % str(_nq))
 
     MyUser.values = {
         "ids": [random.randint(1000000, 10000000) for _ in range(nb)],
         "get_ids": [random.randint(1, 10000000) for _ in range(nb)],
-        "X": utils.generate_vectors(nq, MyUser.op_info["dimension"])
+        "X": utils.generate_vectors(_nq, MyUser.op_info["dimension"])
     }
 
     # MyUser.tasks = {Tasks.query: 1, Tasks.flush: 1}
